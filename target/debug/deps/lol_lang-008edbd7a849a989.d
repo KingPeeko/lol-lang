5b44d9700ecc68acc08fbe9792a42abe3fbf5582
King Peeko <erik.walo@gmail.com>
2025-03-14 12:34:19 +0200

    Added file structure

---
+++ target/debug/deps/lol_lang-008edbd7a849a989.d
@@ -0,0 +1,5 @@
+C:\Users\erikw\Documents\GitHub\lol-lang\target\debug\deps\liblol_lang-008edbd7a849a989.rmeta: src\main.rs
+
+C:\Users\erikw\Documents\GitHub\lol-lang\target\debug\deps\lol_lang-008edbd7a849a989.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/lol_lang-fa8a7c19de69f20e.d
@@ -0,0 +1,5 @@
+C:\Users\erikw\Documents\GitHub\lol-lang\target\debug\deps\liblol_lang-fa8a7c19de69f20e.rmeta: src\main.rs
+
+C:\Users\erikw\Documents\GitHub\lol-lang\target\debug\deps\lol_lang-fa8a7c19de69f20e.d: src\main.rs
+
+src\main.rs:
