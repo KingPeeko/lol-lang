3f1c9abab840914fb443e539ed5b46286bce7835
Roy Långsjö <roy.langsjo@aalto.fi>
2025-04-07 22:11:42 +0300

    chore: remove target/ directory from repo and add .gitignore

--- target/debug/deps/lol_lang-fa8a7c19de69f20e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\erikw\Documents\GitHub\lol-lang\target\debug\deps\liblol_lang-fa8a7c19de69f20e.rmeta: src\main.rs
-
-C:\Users\erikw\Documents\GitHub\lol-lang\target\debug\deps\lol_lang-fa8a7c19de69f20e.d: src\main.rs
-
-src\main.rs:
